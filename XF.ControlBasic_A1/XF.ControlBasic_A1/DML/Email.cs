a433fe3588ce3d42805f4cf3005251b6b674cd55
Jefferson Barros <je_caetano@hotmail.com>
2018-11-03 17:38:33 -0300

    Add project files.

---
+++ XF.ControlBasic_A1/XF.ControlBasic_A1/DML/Email.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XF.ControlesBasicos.DML
+{
+    public class Email : INotifyPropertyChanged
+    {
+        private bool ativar;
+        public bool Ativar
+        {
+            get
+            {
+                return ativar;
+            }
+            set
+            {
+                ativar = value;
+
+                if (!ativar)
+                    EmailInformado = string.Empty;
+            }
+        }
+        private string emailInformado;
+        public string EmailInformado
+        {
+            get
+            {
+                return emailInformado;
+            }
+            set
+            {
+                emailInformado = value;
+                EventPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void EventPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ XF.ControlBasic_A1/XF.ControlBasic_A1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XF.ControlesBasicos.DML;
+
+namespace XF.ControlBasic_A1
+{
+    public partial class MainPage : ContentPage
+    {
+        Email email;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            if (email == null)
+                email = new Email();
+        }
+
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            if (email.Ativar && !string.IsNullOrEmpty(email.EmailInformado))
+                DisplayAlert("Mensagem", $"E-mail enviado para {email.EmailInformado}", "Ok");
+            else
+                DisplayAlert("Mensagem", "Envio não autorizado", "Ok");
+        }
+
+        private void btnConfig_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage() { BindingContext = email });
+        }
+    }
+}
